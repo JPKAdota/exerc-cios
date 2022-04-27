0084602fd954fee169b8529c6b9e1ac1cf3f9f46
João Kadota <joaokadota@gmail.com>
2022-04-27 19:43:47 -0300

    Estudando estrutura de decisões

---
+++ estudos/estrutura de decisões/ex 2.py
@@ -0,0 +1,8 @@
+valor = int(input('Digite um valor: '))
+
+if valor < 0:
+    print('negativo')
+elif valor > 0:
+    print('positivo')
+else:
+    print('neutro')
---
+++ estudos/estrutura de decisões/ex1.py
@@ -0,0 +1,7 @@
+idade = 38
+if idade < 18:
+   print(17)
+elif idade > 30:
+   print(idade)
+else:
+   print(38)
