8c1b68d648494b4ed6aade29914d14d6c1f61913
João Kadota <joaokadota@gmail.com>
2022-04-27 21:33:13 -0300

    Estudando estrutura de decisões

---
+++ estudos/estrutura de decisões/ex4.py
@@ -0,0 +1,15 @@
+letra = input('Digite uma letra: ')
+
+if letra == 'a':
+    print('vogal')
+elif letra == 'e':
+    print('vogal')
+elif letra == 'i':
+    print('vogal')
+elif letra == 'o':
+    print('vogal')
+elif letra == 'u':
+    print('vogal')
+else:
+    print('consoante')
+
